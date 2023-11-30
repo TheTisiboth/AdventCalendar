5335cfa411d4afc57e86057ea4da6a513a903384
Léo Jan <leo.jan@capgemini.com>
2023-11-30 16:09:50 +0100

    Fix starting date

--- src/components/CountdownTimer/ExpiredNotice.tsx
+++ src/components/CountdownTimer/ExpiredNotice.tsx
@@ -1,8 +1,10 @@
+import { Link } from "@tanstack/react-router";
+
 export const ExpiredNotice = () => {
     return (
         <div className="expired-notice">
-            <span>Expired!!!</span>
-            <p>Please select a future date and time.</p>
+            <span>Time has come!!!</span>
+            <p>You can now open your <Link to="/calendar">advent calendar</Link></p>
         </div>
     );
 };
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,15 +1,19 @@
 import { FC, useEffect, useState } from "react";
 import { CountdownTimer } from "./CountdownTimer/CountdownTimer";
+import { Box } from "@mui/material";
 
 export const Home: FC = () => {
     const date = new Date('December 01, 2023 00:00:00');
     const dateTest = new Date('November 30, 2023 14:47:00');
 
 
-    return <div>
-        Home
-        <div>
-            <CountdownTimer targetDate={dateTest} />
-        </div>
-    </div>
+    return <Box
+        style={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            padding: 10
+        }}>
+        <CountdownTimer targetDate={date} />
+    </Box >
 }
