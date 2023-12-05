b102431dee3d347e9709a82bf9cdc5f432dac427
Léo Jan <leo.jan@capgemini.com>
2023-12-05 15:37:21 +0100

    Fix layout on mobile

--- src/components/CalendarComponent.tsx
+++ src/components/CalendarComponent.tsx
@@ -64,14 +64,14 @@ export const CalendarComponent: FC<CalendarComponentProps> = ({ test, resetPictu
                     }
                     {test &&
                         <>
-                            <Grid item xs={9}>
-                                <DayGrid pictures={pictures} test={true} />
-                            </Grid>
-                            <Grid item xs={3}>
-                                <Grid item xs={12}>
+                            <Grid item sm={3}>
+                                <Grid item xs={11} sm={12}>You can simulate todays date with the calendar component. You can also see a representation of an advent calendar that has some closed, ready to be open and opened pictures. A white rectange is a day in the future: you can't interact with it. A blue rectangle is a day in the past, or today: they are ready to be opened. You can interact wiht it by clicking on it: it will open the corresponding picture. You can reset the opened pictures, by clicking on the rest pictures button, on the top left hand side.</Grid>
+                                <Grid item sm={12}>
                                     <DateCalendar value={dayjs(context.date)} onChange={handleCalendarChange} defaultValue={dayjs("2023-12-01")} maxDate={dayjs("2023-12-24")} minDate={dayjs('2023-12-01')} views={["day"]} />
                                 </Grid>
-                                <Grid item xs={12}>You can simulate todays date with the calendar component just above. On the left hand side is a representation of an advent calendar, and has some closed, ready to be open and opened pictures. A white rectange is a day in the future: you can't interact with it. A blue rectangle is a day in the past, or today: they are ready to be opened. You can interact wiht it by clicking on it: it will open the corresponding picture. You can reset the opened pictures, by clicking on the rest pictures button, on the top left hand side.</Grid>
+                            </Grid>
+                            <Grid item xs={10} sm={9}>
+                                <DayGrid pictures={pictures} test={true} />
                             </Grid>
                         </>
                     }
