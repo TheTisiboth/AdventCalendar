313585c010eacad5e798bf0f9773579443ca00e8
Léo Jan <leo.jan@capgemini.com>
2023-12-09 22:29:07 +0100

    Fix PWA settings for automatic download of new version

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { App } from './App'
 import './index.css'
+import { registerSW } from 'virtual:pwa-register'
 
 const queryClient = new QueryClient()
+// Reload the PWA when a new version is available
+registerSW({ immediate: true })
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
