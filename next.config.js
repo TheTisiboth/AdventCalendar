2a82c99157fa9f28cc5956bf4d44ac4b8bafc172
Léo Jan <leo.jan@doctolib.com>
2025-11-30 12:07:18 +0100

    increase body size limit for middleware

--- next.config.js
+++ next.config.js
@@ -1,18 +1,19 @@
-import withPWA from '@ducanh2912/next-pwa';
+import withPWA from "@ducanh2912/next-pwa"
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    serverActions: {
-      bodySizeLimit: '100mb',
+    experimental: {
+        serverActions: {
+            bodySizeLimit: "100mb"
+        },
+        middlewareClientMaxBodySize: "100mb"
     },
-  },
-  output: 'standalone',
+    output: "standalone"
 }
 
 export default withPWA({
-  dest: 'public',
-  disable: process.env.NODE_ENV === 'development',
-  register: true,
-  skipWaiting: true,
-})(nextConfig);
+    dest: "public",
+    disable: process.env.NODE_ENV === "development",
+    register: true,
+    skipWaiting: true
+})(nextConfig)
